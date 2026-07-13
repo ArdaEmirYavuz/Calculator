aad9c2742f82c0e5ff8ccd4c9b13683a2a11e9fe
Arda Emir Yavuz <ardaemiryavuz@hotmail.com>
2026-07-13 22:33:11 +0300

    Add Modules.py to utilize HesapMakinesi functions

---
+++ Modules.py
@@ -0,0 +1,9 @@
+import HesapMakinesi
+sum = HesapMakinesi.Sum([1,2,3,4,5,6,7,8,9,10])
+extract = HesapMakinesi.Extract([1,2,3])
+impact = HesapMakinesi.İmpact([1,2,3,4,5])
+divide = HesapMakinesi.Divide([8,0,2])
+comb = HesapMakinesi.Comb()
+powNum = HesapMakinesi.Pow()
+sqrtNum = HesapMakinesi.Sqrt()
+print(sum,extract,impact,divide,comb,powNum,sqrtNum,sep="\n")
